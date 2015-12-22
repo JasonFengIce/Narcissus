67b90a6829967e0944e487aee59754ae9282ad1e
fenghuibin <fenghuibin@ismartv.cn>
2015-12-22 15:22:39 +0800

    添加 Setting 界面

--- app/src/main/java/cn/ismartv/voice/ui/activity/SettingActivity.java
+++ app/src/main/java/cn/ismartv/voice/ui/activity/SettingActivity.java
@@ -1,16 +1,26 @@
 package cn.ismartv.voice.ui.activity;
 
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
+import android.support.v7.app.AppCompatActivity;
+
+import cn.ismartv.voice.R;
+import cn.ismartv.voice.ui.fragment.SettingFragment;
 
 /**
  * Created by huaijie on 12/22/15.
  */
-public class SettingActivity extends PreferenceActivity {
+public class SettingActivity extends AppCompatActivity {
+    private static final String SETTING_FRAGMENT_TAG = "SETTING_FRAGMENT";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+
+        if (savedInstanceState != null) {
+
+        } else {
+            getFragmentManager().beginTransaction().add(R.id.content, new SettingFragment()).commit();
+        }
     }
-
-
 }
---
+++ app/src/main/java/cn/ismartv/voice/ui/fragment/SettingFragment.java
@@ -0,0 +1,30 @@
+package cn.ismartv.voice.ui.fragment;
+
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+
+import cn.ismartv.voice.R;
+
+/**
+ * Created by huaijie on 12/22/15.
+ */
+public class SettingFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.setting);
+
+
+
+
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+}
