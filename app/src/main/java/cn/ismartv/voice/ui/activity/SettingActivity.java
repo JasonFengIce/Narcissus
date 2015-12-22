9de158923fbc85088f29933645dcac1911c2603a
fenghuibin <fenghuibin@ismartv.cn>
2015-12-22 11:06:01 +0800

    init project

---
+++ app/src/main/java/cn/ismartv/voice/ui/activity/SettingActivity.java
@@ -0,0 +1,16 @@
+package cn.ismartv.voice.ui.activity;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+/**
+ * Created by huaijie on 12/22/15.
+ */
+public class SettingActivity extends PreferenceActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
