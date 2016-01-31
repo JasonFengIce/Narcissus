150f68090337d52bee1b608db913cf52ab67b755
fenghuibin <fenghuibin@ismartv.cn>
2016-01-31 12:08:03 +0800

    add search loading

---
+++ app/src/main/java/cn/ismartv/voice/ui/fragment/SearchLoadingFragment.java
@@ -0,0 +1,30 @@
+package cn.ismartv.voice.ui.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by huaijie on 1/29/16.
+ */
+public class SearchLoadingFragment extends BaseFragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+
+}
