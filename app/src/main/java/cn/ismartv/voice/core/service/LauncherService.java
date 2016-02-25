9b49b934dbff5cae86b662a7f212459074b2ff07
fenghuibin <fenghuibin@ismartv.cn>
2016-02-25 13:26:50 +0800

    Fix Bug ## 修改启动service

--- app/src/main/java/cn/ismartv/voice/core/service/LauncherService.java
+++ app/src/main/java/cn/ismartv/voice/core/service/LauncherService.java
@@ -4,11 +4,14 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 /**
  * Created by huaijie on 2/23/16.
  */
 public class LauncherService extends Service {
+    private static final String TAG = "LauncherService";
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -17,8 +20,10 @@ public class LauncherService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(TAG, "start service");
         Intent i = new Intent();
         i.setAction("cn.ismartv.voice.home");
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(i);
         return super.onStartCommand(intent, flags, startId);
 
